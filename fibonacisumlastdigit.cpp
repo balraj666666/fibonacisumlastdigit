cf97e723da78d2503543d66b83e3af4ecac670cd
balraj <gbalraj2000@gmail.com>
2020-07-20 19:45:52 +0530

    initial commit

---
+++ fibonacisumlastdigit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    long long i;
+    long long a[n+3];
+    a[0]=0;
+    a[1]=1;
+    for(i=2;i<=n+2;i++)
+    {
+        a[i]=(a[i-1]+a[i-2])%10;
+        //cout<<a[i]<<endl;
+    }
+    cout<<(a[n+2]-1)%10<<endl;
+    
+    return 0;
+}
